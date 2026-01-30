57e4bce80dbb6f9307643addef3606a5736bb1cd
Yash Upadhyay <yash.upadhyay2022@glbajajgroup.org>
2026-01-31 01:31:48 +0530

    Cell class Committed

---
+++ src/LLDCaseStudies/tictactoe/dataclasses/Cell.java
@@ -0,0 +1,12 @@
+package LLDCaseStudies.tictactoe.dataclasses;
+
+import LLDCaseStudies.tictactoe.enums.Symbol;
+
+public class Cell {
+
+    public Symbol symbol;
+
+    public Cell(Symbol symbol) {
+        this.symbol = symbol;
+    }
+}
