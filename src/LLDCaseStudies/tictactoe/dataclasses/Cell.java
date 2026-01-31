1a088141530aadbd8c54a509ecc838ff2823f0b7
Yash Upadhyay <yash.upadhyay2022@glbajajgroup.org>
2026-01-31 15:08:11 +0530

    updated Cell class Committed

--- src/LLDCaseStudies/tictactoe/dataclasses/Cell.java
+++ src/LLDCaseStudies/tictactoe/dataclasses/Cell.java
@@ -6,7 +6,18 @@ public class Cell {
 
     public Symbol symbol;
 
-    public Cell(Symbol symbol) {
+    public Cell() {
+        this.symbol = Symbol.EMPTY;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(Symbol symbol) {
         this.symbol = symbol;
+    }
+
+    public boolean isEmpty() {
+        return symbol == Symbol.EMPTY;
     }
 }
